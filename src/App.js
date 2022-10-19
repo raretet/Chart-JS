fbb753a85e61c884a0ca806684fefbb349abeb32
macbook <macbook@MacBook-Pro-macbook.local>
2022-10-19 23:45:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
+import PieChart from './components/PieChart';
+import { UserData } from './Data'
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: 'Users Gained',
+      data: UserData.map((data) => data.userGain),
+      backgroundColor: [
+        'orange',
+        'gray',
+        'aqua',
+        'navy blue',
+        'green',
+        'red',
+        'olive'
+      ]
+    }]
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarChart chartData={userData}/>
+      <LineChart chartData={userData}/>
+      <div className="pieChart">
+        <PieChart chartData={userData}/>
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,44 @@
+export const UserData =  [
+    {
+        id: 1,
+        year: 2015,
+        userGain: 80000,
+        userLost: 823,
+    },
+    {
+        id: 2,
+        year: 2016,
+        userGain: 23000,
+        userLost: 114,
+    },
+    {
+        id: 3,
+        year: 2017,
+        userGain: 24540,
+        userLost: 943,
+    },
+    {
+        id: 4,
+        year: 2018,
+        userGain: 17340,
+        userLost: 928,
+    },
+    {
+        id: 5,
+        year: 2019,
+        userGain: 34800,
+        userLost: 384,
+    },
+    {
+        id: 6,
+        year: 2020,
+        userGain: 83482,
+        userLost: 483,
+    },
+    {
+        id: 7,
+        year: 2021,
+        userGain: 12834,
+        userLost: 129,
+    },
+]
